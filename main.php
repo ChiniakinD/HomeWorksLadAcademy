725e94cb3d5c82b9de4ec7695dee6a5b0c9945f3
ChiniakinD <147251677+ChiniakinD@users.noreply.github.com>
2024-03-06 13:04:09 +0300

    Решение всех задач четвертого дз

--- main.php
+++ main.php
@@ -1,17 +1,23 @@
 <?php
-    require "service/tasks.php";
-    echo vendor_code(54) . PHP_EOL; // Задача 1. Артикул
-    echo convert_time(20490) . PHP_EOL; //Задача 2. Часы
-    echo convert_full_name("Чинякин Кастет Павлович") . PHP_EOL; //Задача 3. ФИО
-    echo check_password(19453632) . PHP_EOL; //Задача 4. Короткий пароль
-    echo check_space("2133 12") . PHP_EOL; // Задача 5. Пробел
-    var_dump(same_numbers_of_letters('fbd15djfi2uvu','fgvdweh21sv')) . PHP_EOL; // Задача 6. Ровно
-    echo more_details('wywuevyufewrfniofnionionwiolnfasauidnqoweopsuifbdsufisbu') . PHP_EOL;// Задача 7. Подробнее
-    echo shorter("Обороноспособность") . PHP_EOL; // Задача 8. Короче
-    echo same_char('qwertyee','e') . PHP_EOL; //Задача 9. Коровы
-    echo check_capsLock('QWERTY123','qwerty123') . PHP_EOL; // Задача 10. Caps Lock
-    echo same_char_without_register('qwErtyeE','e') . PHP_EOL; // Задача 11. Регистронезависимые коровы
-    echo convert_italics_font('aa bb aA cc hf', 'Aa') . PHP_EOL; // Задача 12. Курсив
-    echo tag('<p>Lorem ipsum dolor.</p>') . PHP_EOL; // Задача 13. Тег
-    echo remove_comments('Lorem ipsum dolor./*Lorem ipsum dolor.*/ Lorem ipsum dolor.') . PHP_EOL; // Задача 14. Без комментариев
-    echo search("This is the first sentence. The second sentence contains the keyword 'keyword'. The third sentence.","keyword") . PHP_EOL; // Задача 15. Поиск
+	require "service/tasks.php";
+	print_r(reputation()); // Задача 1. Просто
+	echo searchByKey(reputation(), 'Вася') . PHP_EOL; //Задача 2. Поиск данных
+	echo glossary(reputation(), 'Kastet') . PHP_EOL; // Задача 3. Глоссарий
+	$array = reputation();
+	checkMaxReputation($array) . PHP_EOL; // Задача 4. Выше крыши
+	echo defineReputation(reputation(), 'Вася') . PHP_EOL; // Задача 5. Данила Мастер
+	$array = reputation();
+	checkReputation($array, 'Kastet2'); // Задача 6. Рейтинг +1
+	print_r($array);
+	$array = reputation();
+	deleteNegativeUsers($array, 'Петя'); // Задача 7. Бан
+	print_r($array);
+	echo getNameOfFileAndFolder('/путь/к/файлу/ваш_файл.txt') . PHP_EOL; // Задача 8. Файл
+	print_r(getUserWithMaxReputation($array));    // Задача 9. Царь горы
+	print_r($array);
+	echo changeEnglishToRussia("Today is Monday and tomorrow is Tuesday.") . PHP_EOL; // Задача 10. День недели
+	echo defineSeason('January') . PHP_EOL; // Задачник 11. Лето
+	$cart = [13 => ['name' => 'Кеды', 'count' => 2, 'price' => 123],
+		28 => ['name' => 'Самолет', 'count' => 1, 'price' => 9999999]];
+	echo getPriceOfPurchase($cart, 13) . PHP_EOL; // Задача 12. Корзина
+
--- service/tasks.php
+++ service/tasks.php
@@ -1,145 +1,138 @@
 <?php
-    // Задача 1. Артикул
-    function vendor_code($number): string
-    {
-        return sprintf('%06d', $number);
-    }
-
-    //Задача 2. Часы
-    function convert_time($seconds): string
-    {
-        $hours = floor($seconds / 3600);
-        $minutes = ($seconds - $hours * 3600) / 60;
-        $seconds1 = $seconds % 60;
-        return sprintf("Прошло %d секунд с начала суток, сейчас время %02d:%02d:%02d", $seconds, $hours, $minutes, $seconds1);
-    }
-
-    // Задача 3. ФИО
-    function convert_full_name($full_name): string
-    {
-        $full_name = explode(' ', $full_name);
-        return $full_name[0] . ' ' . mb_substr($full_name[1], 0, 1) . ' ' . mb_substr($full_name[2], 0, 1);
-    }
-
-    // Задача 4. Короткий пароль
-    function check_password($password): string
-    {
-        if (strlen($password) < 8) {
-            return 'Слишком короткий пароль!';
-        }
-        return 'Пароль принят';
-    }
-
-    // Задача 5. Пробел
-    function check_space($password): string
-    {
-        if (substr_count($password, ' ') != 0) {
-            return sprintf('Убери пробелы из пароля');
-        }
-        return 'Пароль принят';
-    }
-
-    // Задача 6. Ровно
-    function same_numbers_of_letters($word1, $word2): bool
-    {
-        $word1 = explode(' ', $word1);
-        $word2 = explode(' ', $word2);
-        $count1 = 0;
-        $count2 = 0;
-        foreach ($word1 as $symbol) {
-            if (ctype_alpha($symbol)) {
-                $count1++;
-            }
-        }
-        foreach ($word2 as $symbol) {
-            if (ctype_alpha($symbol)) {
-                $count2++;
-            }
-        }
-        return $count1 === $count2;
-    }
-
-    // Задача 7. Подробнее
-    function more_details($text): string
-    {
-        if (mb_strlen($text) > 50) {
-            $text = substr($text, 0, 50) . "<a href=#> Подробнее</a>";
-        }
-        return $text;
-    }
-
-    // Задача 8. Короче
-    function shorter($word): string
-    {
-        if (mb_strlen($word) > 7) {
-            $word = mb_substr($word, 0, 4) . '-' . mb_substr($word, -2, 2);
-        }
-        return $word;
-    }
-
-    // Задача 9. Коровы
-    function same_char($word, $char): int
-    {
-        $count = 0;
-        $word = mb_str_split($word);
-        foreach ($word as $letter) {
-            if ($letter === $char) {
-                $count++;
-            }
-        }
-        return $count;
-    }
-
-    // Задача 10. Caps Lock
-    function check_capsLock($password, $true_password): string
-    {
-        return $password === $true_password ? "Добро пожаловать!" : (($password === strtoupper($true_password)) ? "Возможно нажата клавиша CapsLock" : "Ошибка входа");
-    }
-
-    // Задача 11. Регистронезависимые коровы
-    function same_char_without_register($word, $char): int
-    {
-        $word = mb_str_split(strtoupper($word));
-        $char = strtoupper($char);
-        $count = 0;
-        foreach ($word as $letter) {
-            if ($letter === $char) {
-                $count++;
-            }
-        }
-        return $count;
-    }
-
-    // Задача 12. Курсив
-    function convert_italics_font($text, $word): string
-    {
-        $text = strtoupper($text);
-        $word = strtoupper($word);
-        return str_ireplace($word, "<i>$word</i>", $text);
-    }
-
-    // Задача 13. Тег
-    function tag($text): string
-    {
-        return str_replace(array('<p>', '</p>'), '', $text);
-    }
-
-    // Задача 14. Без комментариев
-    function remove_comments($text): string
-    {
-        return str_replace(substr($text, strpos($text, '/*'), strpos($text, '*/') + 2 - strpos($text, '/*')), '', $text);
-    }
-
-    // Задача 15. Поиск
-    function search($text, $word): string
-    {
-        $text = preg_split('/[.,;?!]/', $text);
-        foreach ($text as $sentence) {
-            if (stripos($sentence, $word) !== false) {
-                return str_ireplace($word, "<strong>$word</strong>", $sentence);
-            }
-        }
-        return "";
-    }
-
-
+	// Задача 1. Просто
+	function reputation(): array
+	{
+		return array('Вася' => 37, 'Даша' => 56, 'Kastet' => 100500, 'Петя' => -23, 'Леша' => 67, 'Кирилл' => 45);
+	}
+	
+	// Задача 2. Поиск данных
+	function searchByKey($array, $key): string
+	{
+		foreach ($array as $key_of_element => $element) {
+			if ($key_of_element === $key) {
+				return 'Такой ключ есть';
+			}
+		}
+		return 'Ключ отсутсвует';
+	}
+	
+	// Задача 3. Глоссарий
+	function glossary($array, $key): string
+	{
+		foreach ($array as $key_of_element => $element) {
+			if ($key_of_element === $key) {
+				return $key_of_element . ' ' . $element;
+			}
+		}
+		return 'Данные не найдены';
+	}
+	
+	// Задача 4. Выше крыши
+	function checkMaxReputation(&$array): void
+	{
+		foreach ($array as &$element) {
+			if ($element > 100) {
+				$element = 100;
+			}
+		}
+		print_r($array);
+	}
+	
+	// Задача 5. Данила Мастер
+	function defineReputation($array, $key): string
+	{
+		foreach ($array as $key_of_element => $element) {
+			if ($key_of_element === $key) {
+				$user = $element;
+			}
+		}
+		if ($user < 31) {
+			return 'Новичок';
+		} elseif ($user < 61) {
+			return 'Мастер';
+		} else {
+			return 'Грандмастер';
+		}
+	}
+	
+	// Задача 6. Рейтинг +1
+	function checkReputation(&$array, $key): void
+	{
+		if (isset($array[$key])) {
+			$array[$key]++;
+		} else {
+			$array[$key] = 0;
+		}
+	}
+	
+	// Задача 7. Бан
+	function deleteNegativeUsers(&$array, $key): void
+	{
+		if ($array[$key] < 0) {
+			unset($array[$key]);
+		}
+	}
+	
+	// Задача 8. Файл
+	function getNameOfFileAndFolder($path): string
+	{
+		$arr = explode('/', $path);
+		$file_name = end($arr);
+		$folder_name = $arr[count($arr) - 2];
+		return $folder_name . ' ' . $file_name;
+	}
+	
+	// Задача 9. Царь горы
+	function getUserWithMaxReputation($array): array
+	{
+		arsort($array);
+		return array_slice($array, 0, 3);
+	}
+	
+	// Задача 10. День недели
+	function changeEnglishToRussia($text): string
+	{
+		$dict = [
+			'Monday' => 'Понедельник',
+			'Tuesday' => 'Вторник',
+			'Wednesday' => 'Среда',
+			'Thursday' => 'Четверг',
+			'Friday' => 'Пятница',
+			'Saturday' => 'Суббота',
+			'Sunday' => 'Воскресенье'
+		];
+		return str_replace(array_keys($dict), $dict, $text);
+	}
+	
+	// Задачник 11. Лето
+	function defineSeason($month): string
+	{
+		$array_months = [
+			'January' => 'Winter',
+			'February' => 'Winter',
+			'March' => 'Spring',
+			'April' => 'Spring',
+			'May' => 'Spring',
+			'June' => 'Summer',
+			'July' => 'Summer',
+			'August' => 'Summer',
+			'September' => 'Autumn',
+			'October' => 'Autumn',
+			'November' => 'Autumn',
+			'December' => 'Winter'
+		];
+		return $array_months[$month];
+	}
+	
+	// Задача 12. Корзина
+	function getPriceOfPurchase($basket, $id): int
+	{
+		if (isset($basket[$id])) {
+			return $basket[$id]['count'] * $basket[$id]['price'];
+		}
+		return 0;
+	}
+ 
+ 
+ 
+ 
