b5e3b949df0d5528b8718c3e6990d289ea81feb7
ChiniakinD <147251677+ChiniakinD@users.noreply.github.com>
2024-03-12 09:40:22 +0300

    Решение всех задач кроме последней по теме "Взаимодействие с пользователем"

--- main.php
+++ main.php
@@ -1,18 +1,7 @@
 <?php
 	require "tasks/tasks.php";
-	printText('123', 4) . PHP_EOL; // Задача 1. Слава КПСС
-	printParagraph('привет', 4); // Задача 2. Абзац
-	echo half(500, 50) . PHP_EOL; // Задача 3. Половина
-	$arrayOfReputation = reputation();
-	echo sumOfReputation($arrayOfReputation) . PHP_EOL; // Задача 5. Весь рейтинг
-	echo averageReputation($arrayOfReputation) . PHP_EOL; // Задача 6. Средняя по больнице
-	checkMaxReputation($arrayOfReputation); // Задача 7. Выше крыши 2
-	print_r($arrayOfReputation);
-	checkNegativeUsers($arrayOfReputation); // Задача 8. Второе дно
-	print_r($arrayOfReputation);
-	showMoreThan50($arrayOfReputation); // Задача 9. Экватор
-	usersAndStatus($arrayOfReputation); // Задача 10. Данила Мастер 2
-	$arrayOfReputation2 = reputation1();;
-	print_r(removeSameUsers($arrayOfReputation, $arrayOfReputation2)); // Задача 11. Выйди вон
+
+	
+	
 	
 
--- tasks/tasks.php
+++ tasks/tasks.php
@@ -1,100 +1,217 @@
 <?php
-	// Вспомогательная функция для массива репутации
-	function reputation(): array
+	// Создание вспомогательного массива
+	function getTerms(): array
 	{
-		return array('Вася' => 37, 'Даша' => 56, 'Kastet' => 100500, 'Петя' => -23, 'Леша' => 67, 'Кирилл' => 45, 'Мефодий' => 0, 'Леонид' => -15);
+		$terms = ['Глоссарий' => 'Словарь узкоспециализированных терминов в какой-либо отрасли с толкованием',
+			'Синоним' => 'В языкознаниии слово, совпадающее или близкое по значению с другим словом',];
+		return $terms;
 	}
-	function reputation1(): array
+	
+	// Задача 1. Гость
+	function guest(): void
 	{
-		return array('Вася1' => 37, 'Даша' => 56, 'Kastet1' => 100500, 'Петя' => -23, 'Леша1' => 67, 'Кирилл' => 45, 'Мефодий' => 0, 'Леонид' => -15);
+		if (isset($_GET['name'])) {
+			echo 'Привет ' . htmlspecialchars($_GET['name']) . '!';
+		} else {
+			echo 'Привет Гость!';
+		}
 	}
-	//Задача 1. Слава КПСС
-	function printText($text, $k) : void {
+	
+	// Задача 2. Снова Глоссарий
+	function getDefinitionByVar(): void
+	{
+		$terms = getTerms();
+		if (isset($_GET['term']) && array_key_exists($_GET['term'], $terms)) {
+			echo $terms[$_GET['term']];
+		} else {
+			echo 'Определение отсутсвует';
+		}
+	}
+	
+	// Задача 3. О нас
+	function severalPage(): void
+	{
+		$pages = [
+			'home' => [
+				'title' => 'Главная',
+				'content' => 'Это текст главной страницы',
+			],
+			'about' => [
+				'title' => 'О нас',
+				'content' => 'Это текст с информацией о деятельности владельцев сайта',
+			],
+		];
+		foreach ($pages as $key => $value) {
+			echo "<a href='index.php?page=$key'> {$value['title']}</a>" . PHP_EOL;
+		}
 		
-		for ($i = 0; $i < $k; $i++) {
-			echo $text . ' ';
+		$page = $_GET['page'] ?? 'home';
+		
+		if (isset($pages[$page])) {
+			$pageTitle = $pages[$page]['title'];
+			$pageContent = $pages[$page]['content'];
+		} else {
+			$pageTitle = "Главная отсутствует";
+			$pageContent = "Контент отсутствует";
 		}
-	}
-	//Задача 2. Абзац
-	function printParagraph($text, $k) : void {
-		for ($i = 1; $i <= $k; $i++) {
-			print $text . ' ' . $i . PHP_EOL;
-		}
-	}
-	// Задача 3. Половина
-	function half($startSum, $limitSum) : int {
-		$days = 0;
-		while ($startSum > $limitSum) {
-			$startSum = $startSum / 2;
-			$days++;
-		}
-		return $days;
-	}
-	// Задача 4. Кто есть кто
-	// Задача 5. Весь рейтинг
-	function sumOfReputation($array) : int {
-		$sum = 0;
-		foreach ($array as $reputation) {
-			$sum = $sum + $reputation;
-		}
-		return $sum;
+		
+		echo "<h1>$pageTitle</h1>";
+		echo "<p>$pageContent</p>";
 	}
 	
-	// задача 6. Средняя по больнице
-	function averageReputation($array) : float {
-		$average  = 0;
-		$amount = count($array);
-		foreach ($array as $reputation) {
-			if ($reputation != 0) {
-				$average += $reputation;
-			} else {
-				$amount--;
+	// Задача 4. Опять глоссарий
+	function changeWordByReference($text, $array): void
+	{
+		$text = explode(' ', $text);
+		foreach ($text as $key => $word) {
+			if (array_key_exists($word, $array)) {
+				$text[$key] = str_replace($word, "<a href= 'index.php?term={$word}'> {$word} </a>", $word);
 			}
 		}
-		if ($amount == 0) {
-			return 0;
-		}
-		return $average / $amount;
-	}
-	// Задача 7. Выше крыши 2
-	function checkMaxReputation(&$array) : void {
-		foreach ($array as &$reputation) {
-			if ($reputation >= 100) {
-				$reputation = 100;
-			}
-		}
-	}
-	// Задача 8. Второе дно
-	function checkNegativeUsers(&$array) : void {
-		foreach ($array as &$reputation) {
-			if ($reputation < 0) {
-				$reputation = 0;
-			}
-		}
-	}
-	// Задача 9. Экватор
-	function showMoreThan50($array) : void {
-		foreach ($array as $key => $reputation) {
-			if ($reputation > 50) {
-				echo $key. ' ' . $reputation . PHP_EOL;
-			}
-		}
-	}
-	// Задача 10. Данила Мастер 2
-	function usersAndStatus($array) : void {
-		foreach ($array as $key => $reputation) {
-			if ($reputation <= 30) {
-				$status = 'Новичок';
-			} elseif ($reputation <= 60) {
-				$status = 'Мастер';
-			} else {
-				$status = 'Грандмастер';
-			}
-			echo $key . ' ' . $reputation . ' ' . $status . PHP_EOL;
-		}
-	}
-	// Задача 11. Выйди вон
-	function removeSameUsers($array1, $array2) : array {
-		return array_diff_key($array1,$array2);
+		echo implode(' ', $text);
+		echo "<br>";
+		getDefinitionByVar();
 	}
 	
+	// Задача 5. Калькулятор
+	function calculateSections($area, $power): int
+	{
+		$sections = $area * 100 / $power;
+		return ceil($sections);
+	}
+	
+	function formForCalculateSections(): string
+	{
+		return '
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Калькулятор</title>
+    </head>
+    <body>
+        <h2>Калькулятор количества секций радиатора</h2>
+        <form method="POST">
+            <label for="area">Площадь помещения (кв. м):</label><br>
+            <input type="number" id="area" name="area" placeholder="Площадь" required><br>
+            <label for="power">Мощность одной секции (Вт):</label><br>
+            <input type="number" id="power" name="power" placeholder="Мощность" required><br><br>
+            <input type="submit" value="Рассчитать">
+        </form>
+    </body>
+    </html>';
+	}
+	
+	// Задача 6. Подсветка
+	function formForBacklight(): string
+	{
+		return '<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+        		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+       			<title>Выделение</title>
+			</head>
+			<body>
+				<h2> Выделение текста </h2>
+				<form method="POST">
+					<label for="text"> Введи текст</label><br>
+					<textarea type="text" id = "text" name = "text" rows = "4" cols ="40" required></textarea><br>
+					<input type="submit" value="Запуск">
+				</form>
+			</body>
+		</html>';
+	}
+	
+	function highlightedRed($text, $array): void
+	{
+		$array = preg_split('/\s+/', $array);
+		$text = preg_split('/\s+/', $text);
+		
+		foreach ($text as &$value) {
+			if (in_array($value, $array)) {
+				$value = '<span style="color: red;">' . $value . '</span>';
+			}
+		}
+		echo implode(' ', $text);
+	}
+	
+	// Задача 7. Авторизация
+	function formForAuthorization(): string
+	{
+		return '<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+        		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+       			<title>Форма входа</title>
+			</head>
+			<body>
+				<h2> Авторизация </h2>
+				<form method="POST">
+					<label for="firstname"> Введите логин </label><br>
+					<input type="text" id = "firstname" name = "firstname" required></input><br>
+					<label for="password"> Введите пароль </label><br>
+					<input type="text" id = "password" name = "password" required></input><br><br>
+					<input type="submit" value="Войти">
+				</form>
+			</body>
+		</html>';
+	}
+	
+	function forHeadPage($firstname): string
+	{
+		return '<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+        		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+       			<title>Главная страница</title>
+			</head>
+			<body>
+				<h2>Добрый день' . $firstname . '</h2>
+				<a href = "index.php?action=logout">
+					<button type="button" value="Выйти">Выйти</button>
+				</a>
+			</body>
+		</html>';
+	}
+	
+	function checkAuthorization($firstname, $password, $true_firstname, $truePassword): void
+	{
+		session_start();
+		if ($firstname == $true_firstname && $password == $truePassword) {
+			$_SESSION['firstname'] = $firstname;
+			$_SESSION['password'] = $password;
+			header('Location: http://127.0.0.1/index.php?action=main');
+			echo forHeadPage($firstname);
+		} else {
+			echo formForAuthorization();
+			echo 'Данные для входа неверны';
+		}
+	}
+	
+	function exitFromUser()
+	{
+		session_abort();
+		if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+			unset($_SESSION['firstname']);
+			unset($_SESSION['password']);
+			echo formForAuthorization();
+		}
+	}
+	
+	function switchPage()
+	{
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			checkAuthorization($_POST['firstname'], $_POST['password'], 123, 124);
+		} else {
+			echo formForAuthorization();
+		}
+		exitFromUser();
+	}
+	
+
+
+ 
